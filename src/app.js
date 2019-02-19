a47b0acb202f3c53d1a55ce0a14a9f7dcc7e8441
HARDCOREAJ <565332342@qq.com>
2019-02-19 22:55:53 +0800

    添加Karma自动测试框架&&使用Mocha和Sion做单元测试

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './buttonGroup'
 import chai from 'chai'
+import spy from 'chai-spies'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -18,77 +19,102 @@ new Vue({
 })
 
 const expect = chai.expect
+chai.use(spy)
 
 //unit test
-{
+try {
+  {
 
-  var sampleButton = Vue.extend(Button)
-  const vm = new sampleButton({
-    propsData: {
-      icon: 'settings'
-    }
+    var sampleButton = Vue.extend(Button)
+    const vm = new sampleButton({
+      propsData: {
+        icon: 'settings'
+      }
+    })
+    vm.$mount() //让Button组件变成实例然后挂载在Vue中
+
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.equal('#i-settings')
+    vm.$el.remove()
+    vm.$destroy()
+  }
+
+  {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        loading: true
+      }
+    })
+    vm.$mount() //让Button组件变成实例然后挂载在Vue中
+
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+  }
+
+
+
+  {
+    let div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings'
+      }
+    })
+    vm.$mount(div) ////让Button组件变成实例然后挂载在Vue中,需要有div渲染进去才能看到样式表
+
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+  }
+
+  {
+    let div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        iconPosition: 'right'
+      }
+    })
+    vm.$mount(div) ////让Button组件变成实例然后挂载在Vue中,需要有div渲染进去才能看到样式表
+
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+  }
+
+  {
+    var sampleButton = Vue.extend(Button)
+    const vm = new sampleButton({
+      propsData: {
+        icon: 'settings'
+      }
+    })
+    vm.$mount() //让Button组件变成实例然后挂载在Vue中
+
+    let spy = chai.spy(function () { })
+    vm.$on('click', spy)
+    let button = vm.$el
+    button.click()
+    expect(spy).to.have.been.called()
+  }
+} catch (error) {
+  window.errors = [error]
+} finally {
+  window.errors.forEach((error) => {
+    console.error(error.message)
   })
-  vm.$mount() //让Button组件变成实例然后挂载在Vue中
-
-  let useElement = vm.$el.querySelector('use')
-  let href = useElement.getAttribute('xlink:href')
-  expect(href).to.equal('#i-settings')
-  vm.$el.remove()
-  vm.$destroy()
-}
-
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-      loading: true
-    }
-  })
-  vm.$mount() //让Button组件变成实例然后挂载在Vue中
-
-  let useElement = vm.$el.querySelector('use')
-  let href = useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-loading')
-  vm.$el.remove()
-  vm.$destroy()
-}
-
-
-
-{
-  let div=document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings'
-    }
-  })
-  vm.$mount(div) ////让Button组件变成实例然后挂载在Vue中,需要有div渲染进去才能看到样式表
-
-  let svg = vm.$el.querySelector('svg')
-  let {order} =window.getComputedStyle(svg)
-  expect(order).to.eq('1')
-  vm.$el.remove()
-  vm.$destroy()
-}
-
-{
-  let div=document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-      iconPosition:'right'
-    }
-  })
-  vm.$mount(div) ////让Button组件变成实例然后挂载在Vue中,需要有div渲染进去才能看到样式表
-
-  let svg = vm.$el.querySelector('svg')
-  let {order} =window.getComputedStyle(svg)
-  expect(order).to.eq('2')
-  vm.$el.remove()
-  vm.$destroy()
 }
