a6f8811cbd05e87825634a04138471be10e93e4a
HARDCOREAJ <565332342@qq.com>
2019-02-21 23:10:11 +0800

    添加input样式

--- src/icon.vue
+++ src/icon.vue
@@ -1,16 +1,17 @@
 <template>
     <svg class="g-icon">
-      <use :xlink:href="`#i-${name}`"></use>
+      <use :xlink:href="`#icon-${name}`"></use>
     </svg>
 </template>
 <script>
 import'./svg'
 export default {
-    props: ['name']
+    name:"icon",
+    props: ['name'],
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
     .g-icon {width: 1em; height: 1em; }
 </style>
 
