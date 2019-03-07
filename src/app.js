aa17bc582f44d76b1001b9f8d8d793e3e64cc556
HARDCOREAJ <565332342@qq.com>
2019-03-08 01:08:31 +0800

    基本实现col&row组件

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './buttonGroup'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
 
 new Vue({
   el: '#app',
@@ -16,7 +20,7 @@ new Vue({
     loading1: false,
     loading2: true,
     loading3: false,
-    message:'hi',
+    message: 'hi',
   },
   created() {
   },
---
+++ src/col.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="col" :class="[span&&`col-${span}`, offset&&`offset-${offset}`]" :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+        <div  style="border: 1px solid green; height: 100px;">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Col",
+  props: {
+    span: {
+      type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
+    }
+  },
+  data() {
+    return {
+      gutter: 0
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.col {
+  width: 50%; //默认样式为屏幕宽度的一半
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
+}
+</style>
+
+
