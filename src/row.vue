0a289636242664b1608888d19712136f8acf4ade
HARDCOREAJ <565332342@qq.com>
2019-03-10 20:50:08 +0800

    重构一下col&row的样式

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
@@ -13,12 +13,16 @@ export default {
     }
   },
   created() {
-    console.log("row created");
+
   },
   mounted() {
-    console.log("row mounted");
-    console.log(this.$children);
     this.$children.forEach(vm => (vm.gutter = this.gutter));
+  },
+  computed:{
+    rowStyle(){
+      let {gutter} = this
+      return {marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}
+    }
   }
 }
 var div = document.createElement("div"); // created
