9537233051b04b50cec44cdff088c8a20dc42917
HARDCOREAJ <565332342@qq.com>
2019-03-24 23:40:04 +0800

    完善tabs样式

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -21,11 +21,13 @@ export default {
 <style <style lang="scss" scoped>
 $tab-height: 40px;
 $blue:blue;
+$border-color:#ddd;
 .tabs-head {
   display: flex;
   height: $tab-height;
   justify-content: flex-start;
   position: relative;
+  border-bottom: 1px solid $border-color;
   >.line{
     position: absolute;
     bottom: 0;
@@ -34,6 +36,10 @@ $blue:blue;
   }
   > .actions-wrapper {
     margin-left: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 1em;
   }
 }
 </style> 
