6e6dd20b4ed78e8dad102f33e86f33af7cc72200
HARDCOREAJ <565332342@qq.com>
2019-03-24 23:33:31 +0800

    添加tabs切换动画

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -9,10 +9,11 @@
 export default {
   name: "GrowTabsHead",
   inject: ["eventBus"],
-  created() {
+  mounted() {
     this.eventBus.$on('update:selected',(item,vm)=>{
-      console.log(item)
-      console.log(vm)
+      let{width,height,top,left}=vm.$el.getBoundingClientRect()
+      this.$refs.line.style.width=`${width}px`
+      this.$refs.line.style.left=`${left}px`
     })
   }
 };
@@ -24,13 +25,12 @@ $blue:blue;
   display: flex;
   height: $tab-height;
   justify-content: flex-start;
-  border: 1px solid purple;
   position: relative;
   >.line{
     position: absolute;
     bottom: 0;
     border-bottom: 1px solid $blue;
-    width:100px;
+    transition: all 350ms;
   }
   > .actions-wrapper {
     margin-left: auto;
